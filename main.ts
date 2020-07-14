e3031f11d6e7446ecb5a36b64ad1b67cc4ead559
teddycool21 <teddylesmana21@gmail.com>
2020-07-14 11:37:22 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.OnceInBackground)
+    basic.showIcon(IconNames.House)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Forever)
+    basic.showIcon(IconNames.Yes)
 })
